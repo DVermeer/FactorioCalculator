18b016ac22c09fd42c8e55fa02739ae0b476ff2a
DVermeer <44090457+DVermeer@users.noreply.github.com>
2019-06-21 23:36:31 +0200

    needAssembler functio gebouwd

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-console.log('Hello world')
+const desired = 100;
+const copperwireOutput = 2;
+const copperwireTime = 0.5;
 
-console.log('Test Commit')
+function needAssembler(desired, output, timetocomplete) {
+    const result = Math.ceil(desired / (output / timetocomplete));
+    return result;
+}
+const amountAssembler = needAssembler(desired, copperwireOutput, copperwireTime)
+
+console.log('You need ' + amountAssembler + ' Assemblers')
+
